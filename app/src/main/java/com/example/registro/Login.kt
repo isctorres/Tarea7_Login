8048bb280de1b4b72261e3d5e3ef01197aac3803
isctorres <isctorres@gmail.com>
2020-03-10 11:55:14 -0600

    creacion del repositorio

---
+++ app/src/main/java/com/example/registro/Login.kt
@@ -0,0 +1,22 @@
+package com.example.registro
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnCrearCuenta.setOnClickListener(this);
+    }
+
+    override fun onClick(v: View?) {
+        val intSingUp = Intent(this@Login, SignUp::class.java)
+        startActivity(intSingUp)
+    }
+}
